d6e718ec999afae1a7921a2e91ab8f814e28ab09
Edward Wu <wuedward@bitbucket.org>
2015-09-21 17:15:56 -0400

    added json responses

--- smart-alarm-server/app/controllers/users_controller.rb
+++ smart-alarm-server/app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+	skip_before_action :verify_authenticity_token
+
 	def login
 		#render login.html.erb
 	end
@@ -11,10 +13,29 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			flash[:notice] = "User created! The user_id is #{@user.id}"
-			redirect_to(:root)
+			respond_to do |format|
+				format.html {
+					flash[:notice] = "User created! The user_id is #{@user.id}"
+					redirect_to(:root)
+				}
+				format.json {
+					response = Hash.new
+					response['status'] = 'User created!'
+					response['user'] = @user
+					render json: response
+				}
+			end
 		else
-			render('new')
+			respond_to do |format|
+				format.html {
+					render('new')
+				}
+				format.json {
+					response = Hash.new
+					response['status'] = 'Error creating user.'
+					render json: response
+				}
+			end
 		end
 	end
 
@@ -27,20 +48,41 @@ class UsersController < ApplicationController
 					respond_to do |format|
 						format.html {
 							flash[:notice] = "Login successful!"
+							#puts authorized_user.inspect
 							redirect_to(:root)
 						}
 						format.json {
+							#puts authorized_user.inspect
+							#puts "json"
 							render json: authorized_user
 						}
 					end
 				else
-					flash[:notice] = "Invalid email/password."
-					redirect_to(:root)
+					respond_to do |format|
+						format.html {
+							flash[:notice] = "Invalid email/password."
+							redirect_to(:root)
+						}
+						format.json {
+							response = Hash.new
+							response['status'] = 'Invalid email/password'
+							render json: response
+						}
+					end
 				end
 			end
 		else
-			flash[:notice] = "Invalid email/password."
-			redirect_to(:root)
+			respond_to do |format|
+				format.html {
+					flash[:notice] = "Improper request"
+					redirect_to(:root)
+				}
+				format.json {
+					response = Hash.new
+					response['status'] = 'Improper request.'
+					render json: response
+				}
+			end
 		end
 	end
 
